d1ae9454b6ba50e3b94109e213339fe1cfec5da5
shabnam <shabnam.mostary@gmail.com>
2021-06-11 00:56:16 -0400

    added plant links in the garden page

--- components/gardenDetails.jsx
+++ components/gardenDetails.jsx
@@ -2,13 +2,18 @@
 import React from 'react'
 import styled from 'styled-components'
 import { useAuth0 } from '@auth0/auth0-react'
+import { NavLink } from 'react-router-dom'
 import deletePlantFromGarden from '../actions/gardenDelete'
 
+const Link = styled(NavLink) `
+  text-decoration: none;
+  color:black;
+  font-family: Arial, Helvetica, sans-serif;
+`
+
 const Details = styled.div `
-  font-size: 20px;
-  font-weight: 400;
+  font-size: 16px;
   margin: 20px;
-  text-align: center;
   border-top: 1px solid black;
   display:flex;
   flex-direction:row;
@@ -17,15 +22,17 @@ const Details = styled.div `
   color:black ;
      > div {
     padding: 10px;
+    text-align: center;
     margin:20px;
     }
     > div button {
       background:rgb(189, 9, 6);
       border: none;
+      padding:0.4em;
       color:white;
     }
     > div img {
-        width:30%;
+        width:40%;
         height:80%;
     }
 `
@@ -37,9 +44,18 @@ export default ({
 
   return (
     <Details key={`${id}`}>
-      <div><h3>{`${name}`}</h3></div>
       <div>
-        <img src={`https://res-4.cloudinary.com/do6bw42am/image/upload/c_scale,f_auto,h_300/v1/${image}`} alt="plant" />
+        <Link to={`/plants/${name}`}>
+          <h3>
+            {' '}
+            {`${name}`}
+          </h3>
+        </Link>
+      </div>
+      <div>
+        <Link to={`/plants/${name}`}>
+          <img src={`https://res-4.cloudinary.com/do6bw42am/image/upload/c_scale,f_auto,h_300/v1/${image}`} alt="plant" />
+        </Link>
       </div>
       <div>
         <button
